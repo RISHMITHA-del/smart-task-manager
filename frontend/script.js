1ba84c2cf6202f045ce37d641da48e4d3cd9f310
23491A12B8 RAMISETTY RISHMITHA <ramisettyrishmitha1706@gmail.com>
2026-05-22 17:05:44 +0530

    remove package.json

--- frontend/script.js
+++ frontend/script.js
@@ -1,26 +1,61 @@
 function addTask(){
 
     const taskInput = document.getElementById("taskInput");
+    const taskDate = document.getElementById("taskDate");
+    const priority = document.getElementById("priority");
     const taskList = document.getElementById("taskList");
 
     if(taskInput.value === ""){
-        alert("Please enter a task");
+        alert("Enter task");
         return;
     }
 
     const taskCard = document.createElement("div");
     taskCard.classList.add("task-card");
+    taskCard.classList.add(priority.value);
 
-    taskCard.innerHTML = `
-        <span>${taskInput.value}</span>
-        <button class="delete-btn">Delete</button>
-    `;
+    const left = document.createElement("div");
+    left.classList.add("task-left");
 
-    taskCard.querySelector(".delete-btn").addEventListener("click", function(){
+    const top = document.createElement("div");
+    top.classList.add("task-top");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const text = document.createElement("span");
+    text.innerText = taskInput.value;
+
+    checkbox.onchange = function(){
+        if(checkbox.checked){
+            text.style.textDecoration = "line-through";
+        } else {
+            text.style.textDecoration = "none";
+        }
+    }
+
+    top.appendChild(checkbox);
+    top.appendChild(text);
+
+    const dateText = document.createElement("small");
+    dateText.innerText = "Due: " + (taskDate.value || "No date");
+
+    left.appendChild(top);
+    left.appendChild(dateText);
+
+    const del = document.createElement("button");
+    del.innerText = "Delete";
+    del.classList.add("delete-btn");
+
+    del.onclick = function(){
         taskCard.remove();
-    });
+    }
+
+    taskCard.appendChild(left);
+    taskCard.appendChild(del);
 
     taskList.appendChild(taskCard);
 
     taskInput.value = "";
+    taskDate.value = "";
 }
